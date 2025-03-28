c8dab018341682b71832bae2c1dedafe3203b5f9
Zennn27003 <iradielt.@gmail.com>
2025-03-27 20:51:19 -1200

    Add includes/trainer_nav.php 1743223939457

---
+++ includes/trainer_nav.php
@@ -0,0 +1,42 @@
+<div class="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active text-white" href="<?= URL_ROOT ?>/trainer/dashboard">
+                    <i class="fas fa-tachometer-alt me-2"></i>Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="<?= URL_ROOT ?>/trainer/sessions">
+                    <i class="fas fa-calendar-check me-2"></i>Sessions
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="<?= URL_ROOT ?>/trainer/materials">
+                    <i class="fas fa-book-medical me-2"></i>Materials
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">
+                    <i class="fas fa-user-graduate me-2"></i>Trainees
+                </a>
+            </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Account</span>
+        </h6>
+        <ul class="nav flex-column mb-2">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">
+                    <i class="fas fa-user-cog me-2"></i>Profile
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="<?= URL_ROOT ?>/home/logout">
+                    <i class="fas fa-sign-out-alt me-2"></i>Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
