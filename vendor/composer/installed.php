dc52045ca716644244c9d9312ca995bc6181c755
Zennn27003 <iradielt.@gmail.com>
2025-03-27 20:50:46 -1200

    Add vendor/composer/installed.php 1743223906470

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hospital/training-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3e372a2e7e25d1f8e1fa038b9678b202630552b5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hospital/training-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3e372a2e7e25d1f8e1fa038b9678b202630552b5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
